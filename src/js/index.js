7e669c27e482228e58c9ce00ebaa4039027b43f3
Gullrye <wmwspecter@163.com>
2021-08-11 10:36:00 +0800

    上拉加载更多

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,13 @@
 import './imports'
-
 import Header from '../components/Header'
 import NavBar from '../components/NavBar'
-import { NEWS_TYPE } from '../data'
-
-import services from '../services'
-
 import NewsList from '../components/NewsList'
+import PageLoading from '../components/PageLoading'
+import MoreLoading from '../components/MoreLoading'
+
+import { NEWS_TYPE } from '../data'
+import services from '../services'
+import { scrollToBottom } from '../libs/utils'
 
 ;((doc) => {
   const oApp = doc.querySelector('#app')
@@ -15,9 +16,10 @@ import NewsList from '../components/NewsList'
   const config = {
     type: 'top',
     count: 10,
-    pageNum: 0
+    pageNum: 0,
+    isLoading: false
   }
-
+  let t= null
   const newsData = {}
 
   const init = async () => {
@@ -29,6 +31,7 @@ import NewsList from '../components/NewsList'
 
   function bindEvent() {
     NavBar.bindEvent(setType)
+    window.addEventListener('scroll', scrollToBottom.bind(null, getMoreList), false)
   }
 
   function render() {
@@ -51,7 +54,9 @@ import NewsList from '../components/NewsList'
       data,
       pageNum
     })
+    MoreLoading.remove(oListWrapper)
     oListWrapper.innerHTML += NewsListTpl
+    config.isLoading = false
     NewsList.imgShow()
   }
 
@@ -59,17 +64,42 @@ import NewsList from '../components/NewsList'
     const { type, count, pageNum } = config
 
     if (newsData[type]) {
+      renderList(newsData[type][pageNum])
       return
     }
-
+    oListWrapper.innerHTML = PageLoading.tpl()
     newsData[type] = await services.getNewsList(type, count)
     renderList(newsData[type][pageNum])
+    setTimeout(() => {
+      oListWrapper.innerHTML = ''
+      renderList(newsData[type][pageNum])
+    }, 1500)
   }
 
   // NavBar.bindEvent的回调函数
   function setType(type) {
     config.type = type
-    console.log(config.type)
+    config.pageNum = 0
+    config.isLoading = false
+    oListWrapper.innerHTML = ''
+    setNewsList()
+  }
+
+  function getMoreList () {
+    if(!config.isLoading) {
+      config.pageNum++
+      clearTimeout(t)
+      const {pageNum, type} = config
+      if(pageNum >= newsData[type].length) {
+        MoreLoading.add(oListWrapper, false)
+      } else {
+        config.isLoading = true
+        MoreLoading.add(oListWrapper, true)
+        t = setTimeout(() => {
+          setNewsList()
+        }, 1000);
+      }
+    }
   }
 
   init()
